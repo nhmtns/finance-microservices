134659a9c2724cd4f861d35c875744c6cfb97dc8
srini <srini@MintRosa>
2016-08-26 09:31:46 -0400

    First Commit

---
+++ src/main/java/com/srini/finance/FinanceMicroservicesApplication.java
@@ -0,0 +1,12 @@
+package com.srini.finance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceMicroservicesApplication.class, args);
+	}
+}
